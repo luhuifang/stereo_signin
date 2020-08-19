39586cc0262506035b0de05c14f895c611f06fd7
zhou995287902 <zhouliangliang@users.noreply.github.com>
2020-08-19 14:20:51 +0800

    update page layout with boostrap.css
    update page layout with boostrap.css

--- Register_page.py
+++ Register_page.py
@@ -1,158 +1,199 @@
+import re
 import dash
+import json
+import callbacks
+import pandas as pd
+import country as Dis
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
-import plotly.express as px
-import pandas as pd
-import json
-from pymysql import connect
-import country as Dis
-import re
+from spatialTrancriptomeReport import app
 from apps.db.tableService.Users import Users
 from apps.db.tableService.UserRole import UserRole
-from apps.db.tableService.ConnectMYSQL import ConnectMYSQL
-from apps.db.database_settings import MYSQL_LOCAL as MYSQL
-from spatialTrancriptomeReport import app
-import callbacks
+from dash.dependencies import Input, Output, State
 
 
-layout=html.Div(id='father_div',children=[
-    html.H1(children='Register'),
-        html.Form(id='Username_form',children=[
-            html.Label('UserName: ',id='Username_label'),
+layout=html.Form(id='father_div',className="form-group login_form form_bg",children=[
+    html.Div(className="form-group",children=[
+        html.H1(children='Register',className='text-center'),
+        html.Div(id='Username_form',className='row ',children=[
+            html.Label('UserName: ',id='Username_label',htmlFor='Username',className='col-md-offset-4 label_font_size label_before'),
             html.Br(),
-            dcc.Input(
-                id='Username',
-                type='text',
-                value='',
-                placeholder='Enter Username',
-                autoFocus=True,
-                # required=True,
-                # pattern= '^[a-zA-Z]+[a-zA-Z0-9_]*'
+            html.Div(className= 'col-md-3 col-md-offset-4',children=[
+                dcc.Input(
+                    id='Username',
+                    type='text',
+                    value='',
+                    className="form-control input_part",
+                    placeholder='Enter Username',
+                    autoFocus=True,
+                    # required=True,
+                    # pattern= '^[a-zA-Z]+[a-zA-Z0-9_]*'
+                    )
+                ]),
+            # html.Div(id='Username_error'),
+            html.P(
+                id='Username_info',
+                children='Username can contain numbers, letters and underscores, cannot be empty and repeated, and cannot contain spaces and special characters',
+                className='fill_request'
                 ),
-            html.Div(id='Username_error'),
-            ]
-            ),
-        html.Form(id='Password_form',n_clicks=0,children=[
-            html.Label('PassWord: ',id='Password_label'),
+            ]),
+        html.Div(id='Password_form',className='row',n_clicks=0,children=[
+            html.Label('PassWord: ',id='Password_label',htmlFor='Password',className='col-md-offset-4 label_font_size label_before'),
             html.Br(),
-            dcc.Input(
-                id='Password',
-                type='password',
-                minLength= 8,
-                maxLength= 25,
-                value='',
-                # required=True,
-                placeholder='Enter Password',
-                # pattern='^(?![\d]+$)(?![a-zA-Z]+$)(?![_]+$)[\da-zA-Z_]{8,25}$'
+            html.Div(className= 'col-md-3 col-md-offset-4',children=[
+                dcc.Input(
+                    id='Password',
+                    type='password',
+                    className="form-control input_part",
+                    minLength= 8,
+                    maxLength= 25,
+                    value='',
+                    placeholder='Enter Password',
+                    # pattern='^(?![\d]+$)(?![a-zA-Z]+$)(?![_]+$)[\da-zA-Z_]{8,25}$'
+                    )
+                ]),
+            # html.Div(id='Password_error')
+            html.P(
+                id='Password_info',
+                children='The password cannot be empty (8-25 characters), and must contain more than three types of lowercase letters, uppercase letters, numbers and special characters',
+                className='fill_request'
                 ),
-            html.Div(id='Password_error')
-            ]
-            ),
-        html.Form(id='Repassword_form',n_clicks=0,children=[
-            html.Label('RePassWord: ',id='Repassword_label'),
+            ]),
+        html.Div(id='Repassword_form',className='row',n_clicks=0,children=[
+            html.Label('RePassWord: ',id='Repassword_label',htmlFor='RePassword',className='col-md-offset-4 label_font_size label_before'),
             html.Br(),
-            dcc.Input(
-                id='RePassword',
-                type='password',
-                minLength= 8,
-                maxLength= 25,
-                value='',
-                # required=True,
-                placeholder='Re-enter Password',
-                # pattern='^(?![\d]+$)(?![a-zA-Z]+$)(?![_]+$)[\da-zA-Z_]{8,25}$'
-                ),
-            html.Div(id='Repassword_error')
-            ]
-            ),
-        html.Form([
-            html.Label('RealName: ',id='realname_label'),
+            html.Div(className= 'col-md-3 col-md-offset-4',children=[
+                dcc.Input(
+                    id='RePassword',
+                    type='password',
+                    className="form-control input_part",
+                    minLength= 8,
+                    maxLength= 25,
+                    value='',
+                    placeholder='Re-enter Password',
+                    # pattern='^(?![\d]+$)(?![a-zA-Z]+$)(?![_]+$)[\da-zA-Z_]{8,25}$'
+                    )
+                ]),
+                # html.Div(id='Repassword_error')
+            ]),
+        html.Div(className='row',children=[
+            html.Label('RealName: ',id='realname_label',htmlFor='Realname',className='col-md-offset-4 label_font_size'),
             html.Br(),
-            dcc.Input(
-                id='Realname',
-                type='text',
-                value='',
-                placeholder='Enter your Real Name',
-                # pattern= '^[a-zA-Z][a-zA-Z0-9_]*'
-                )
-            ]
-            ),
-        html.Form([
-            html.Label('Email: ',id='Email_label'),
+            html.Div(className= 'col-md-3 col-md-offset-4',children=[
+                dcc.Input(
+                    id='Realname',
+                    className="form-control input_part",
+                    type='text',
+                    value='',
+                    placeholder='Enter your Real Name',
+                    # pattern= '^[a-zA-Z][a-zA-Z0-9_]*'
+                    )
+                ])
+            ]),
+        html.Div(className='row',id="inputEmail3",children=[
+            html.Label('Email: ',id='Email_label',htmlFor='Email',className='col-md-offset-4 label_font_size label_before'),
             html.Br(),
-            dcc.Input(
-                id='Email',
-                # type='email',
-                value='',
-                placeholder='Enter Email Address',
-                # inputMode='email'
-                # pattern= '^[a-zA-Z][a-zA-Z0-9_]*'
-                )
-            ]
-            ),
-        html.Form([
-            html.Label('Phone: ',id='phone_label'),
+            html.Div(className= 'col-md-3 col-md-offset-4',children=[
+                dcc.Input(
+                    id='Email',
+                    # type='email',
+                    className="form-control input_part",
+                    value='',
+                    placeholder='Enter Email Address',
+                    # inputMode='email'
+                    # pattern= '^[a-zA-Z][a-zA-Z0-9_]*'
+                    )
+                ])
+            ]),
+        html.Div(className='row',children=[
+            html.Label('Phone: ',id='phone_label',htmlFor='Phone',className='col-md-offset-4 label_font_size'),
             html.Br(),
-            dcc.Input(
-                id='Phone',
-                type='tel',
-                value='',
-                placeholder='Enter the Telephone',
-                inputMode='numeric',
-                # pattern= '^[0-9]{2}[+]?[0-9]+'
-                )
-            ]
-            ),
-        html.Label('District: ',id= 'District_label'),
-        html.Br(),
-        html.Div([
-            dcc.Dropdown(
-                options=[{'label':i,'value':i} for i in Dis.options.keys()],
-                id='District',
-                value="Asia",
-                persistence=True
-                )
-            ],id='District_div'
-            ),
-        html.Label('Country: ',id='Country_label',
-    ),
-        html.Br(),
-        html.Div([
-            dcc.Dropdown(id='Country')
-            ],id='country_div'
-            ),
-        html.Label('Province: ',id='Province_label',
-    ),
-        html.Br(),
-        html.Div([
-            dcc.Dropdown(id='Province')
-            ],id='province_div'
-            ),
-        html.Label('City: ',id='City_label',
-    ),
-        html.Br(),
-        html.Div([
-            dcc.Dropdown(id='City')
-            ],id='city_div'
-            ),
-        html.Div([
-            html.Label('Organization: ',id='Organization_label'),
+            html.Div(className= 'col-md-3 col-md-offset-4',children=[
+                dcc.Input(
+                    id='Phone',
+                    type='tel',
+                    className="form-control input_part",
+                    value='',
+                    placeholder='Enter the Telephone',
+                    inputMode='numeric',
+                    # pattern= '^[0-9]{2}[+]?[0-9]+'
+                    )
+                ])
+            ]),
+        html.Div(className='row',children=[
+            html.Label('District: ',id= 'District_label',htmlFor='District',className='col-md-offset-4 label_font_size label_before'),
             html.Br(),
-            dcc.Input(
-                id='Organization',
-                type='text',
-                placeholder='Enter Organization',
-                value=''
-                )
-            ],id='Organization_div'
-            # ,style={'position':'relative','bottom':'-150px','float': 'right','width': '60%','height': '40px','digplay':'inline-block','margin': '2px'}
-            ),
-        html.Div(id='button_div',children=[
-            html.Button('Create account',id='create_button',n_clicks=0),
-            html.Button([dcc.Link('Cancel', href='/')],id='Cancel_button',n_clicks=0),
-            html.Button('Reset',id='clear_button',n_clicks=0)
-            ]
-            # ,style=styles['button']
-            ),
-        html.Div(id='account_result')
+            html.Div(className= 'col-md-3 col-md-offset-4',id='District_div',children=[
+                dcc.Dropdown(
+                    options=[{'label':i,'value':i} for i in Dis.options.keys()],
+                    id='District',
+                    value="",
+                    # persistence=True,
+                    placeholder='Select the District',
+                    className=" dropdown_font_size input_part"
+                    )
+                ])
+            ]),
+        html.Div(className='row',children=[
+            html.Label('Country: ',id='Country_label',className='col-md-offset-4 label_font_size label_before'),
+            html.Br(),
+            html.Div(className= 'col-md-3 col-md-offset-4',id='country_div',children=[
+                dcc.Dropdown(
+                    id='Country',
+                    placeholder='Select the Country',
+                    className=" dropdown_font_size input_part"
+                    )
+                ])
+            ]),
+        html.Div(className='row',children=[
+            html.Label('Province: ',id='Province_label',className='col-md-offset-4 label_font_size label_before'),
+            html.Br(),
+            html.Div(className= 'col-md-3 col-md-offset-4',id='province_div',children=[
+                dcc.Dropdown(
+                    id='Province',
+                    placeholder='Select the Province',
+                    className=" dropdown_font_size input_part"
+                    )
+                ])
+            ]),
+        html.Div(className='row',children=[
+            html.Label('City: ',id='City_label',className='col-md-offset-4 label_font_size label_before'),
+            html.Br(),
+            html.Div(className= 'col-md-3 col-md-offset-4',id='city_div',children=[
+                dcc.Dropdown(
+                    id='City',
+                    placeholder='Select the City',
+                    className=" dropdown_font_size input_part"
+                    )
+                ])
+            ]),
+        html.Div(className= 'row',id='Organization_div',children=[
+            html.Label('Organization: ',id='Organization_label',className='col-md-offset-4 label_font_size label_before'),
+            html.Br(),
+            html.Div(className= 'col-md-3 col-md-offset-4',children=[
+                dcc.Input(
+                    id='Organization',
+                    type='text',
+                    placeholder='Enter Organization',
+                    className="form-control input_part",
+                    value=''
+                    )
+                ])
+            ]),
+        html.Div(children=[
+            html.Div(id='account_result',className='text-center text-danger')
+        ]),
+        html.Div(className='row',children=[
+            html.Div(id='button_div',className='row',children=[
+                html.Button('Create account',id='create_button',type='button',n_clicks=0,className="btn-primary col-md-3 col-md-offset-4 login_button")
+                ]),
+            html.Div(className='row',children=[
+                dcc.Link('Rerurn', href='/',id='Cancel_button',className="btn-primary col-md-3 col-md-offset-4 login_button text-center")
+                ]),
+            html.Div(className='row',children=[
+                html.Button('Reset',id='clear_button',type='button',n_clicks=0,className="btn-primary col-md-3 col-md-offset-4 login_button")
+                ])
+        ]),
     ])
+])
+
--- callbacks.py
+++ callbacks.py
@@ -1,64 +1,37 @@
-from dash.dependencies import Input, Output, State
+import re
+import country_dict as Dis
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.exceptions import PreventUpdate
 from spatialTrancriptomeReport import app
-import country_dict as Dis
-import re
 from apps.db.tableService.Users import Users
 from apps.db.tableService.UserRole import UserRole
-from dash.exceptions import PreventUpdate
+from dash.dependencies import Input, Output, State
 
 
 @app.callback(
-    Output('country_div', 'children'),
+    Output('Country', 'options'),
     [Input('District', 'value')]
 )
 def set_neighborhood(District):
+    if not District:
+        raise PreventUpdate
     neighborhoods = [i for i in Dis.options[District]["childrens"].keys()]
-    return dcc.Dropdown(
-        id='Country',
-        value=neighborhoods[0],
-        options=[{'label': v, 'value': v} for v in neighborhoods],
-        persistence_type='session',
-        persistence=District
-            )
-
+    return [{'label': v, 'value': v} for v in neighborhoods]
 @app.callback(
-    Output('province_div', 'children'),
+    Output('Province', 'options'),
     [Input('Country', 'value'),
     Input('District', 'value')]
 )
 def set_province_neighborhood(country,District):
     try:
         province_neigh = [ii for ii in Dis.options[District]["childrens"][country]["childrens"]]
-        # print(Dis.options[District]["childrens"][country]["childrens"])
-        if province_neigh:
-            return dcc.Dropdown(
-                id='Province',
-                value=province_neigh[0],
-                options=[{'label': v, 'value': v} for v in province_neigh],
-                persistence_type='session',
-                persistence=country
-            )
-        else:
-            return dcc.Dropdown(
-                id='Province',
-                value='',
-                options=[{'label': v, 'value': v} for v in province_neigh],
-                persistence_type='session',
-                persistence=country
-            )
+        return [{'label': v, 'value': v} for v in province_neigh]
     except:
-        return dcc.Dropdown(
-                id='Province',
-                value='',
-                options=[],
-                persistence_type='session',
-                persistence=country
-            ) 
+        return [{'label': 'None', 'value': 'None'}]
 
 @app.callback(
-    Output('city_div', 'children'),
+    Output('City', 'options'),
     [Input('Country', 'value'),
     Input('District', 'value'),
     Input('Province','value')]
@@ -66,71 +39,9 @@ def set_province_neighborhood(country,District):
 def set_city_neighborhood(country,District,Province):
     try:
         city_neigh = [ii for ii in Dis.options[District]["childrens"][country]["childrens"][Province]['childrens']]
-        # print(Dis.options[District]["childrens"][country]["childrens"])
-        if city_neigh:
-            return dcc.Dropdown(
-                id='City',
-                value=city_neigh[0],
-                options=[{'label': v, 'value': v} for v in city_neigh],
-                persistence_type='session',
-                persistence=country
-            )
-        else:
-            return dcc.Dropdown(
-                id='City',
-                value='',
-                options=[{'label': v, 'value': v} for v in city_neigh],
-                persistence_type='session',
-                persistence=country
-            )
+        return [{'label': v, 'value': v} for v in city_neigh]
     except:
-        return dcc.Dropdown(
-                id='City',
-                value='',
-                options=[],
-                persistence_type='session',
-                persistence=country
-            ) 
-
-@app.callback(
-    Output('Username_error', 'children'),
-    [Input('Username', 'value'),
-    Input('Username_form','n_clicks')])
-def update_username_info(Username,n_click):
-    conn = Users(username=Username)
-    if not Username:
-        raise PreventUpdate
-    if not re.search(r'^[a-zA-Z]+[a-zA-Z0-9_]*',Username) :
-        return html.Div('The username is incorrect, please re-enter!')
-    elif re.search(r'^[a-zA-Z]+[a-zA-Z0-9_]*',Username):
-        res=conn.checkExists()
-        if res:
-            return html.Div('Username already exists, please re-enter!')
-        else:
-            return html.Div('Username is available!')
-
-@app.callback(
-    Output('Password_error', 'children'),
-    [Input('Password', 'value'),
-    Input('Password_form','n_clicks')])
-def update_username_info(Password,n_click):
-    if not Password:
-        raise PreventUpdate
-    if not re.search(r'^(?![a-zA-z]+$)(?!\d+$)(?![!@#$%^&*.,?]+$)(?![a-z\d]+$)(?![A-Z\d]+$)(?![a-z!@#$%^&*,.?]+$)(?![A-Z!@#$%^&*,.?]+$)(?![\d!@#$%^&*,.?]+$)[a-zA-Z\d!@#$%^&*,.?]{8,25}$',Password):
-        return html.Div('The password input format is wrong, please input according to the rules!')
-
-@app.callback(
-    Output('Repassword_error', 'children'),
-    [Input('RePassword', 'value'),
-    Input('Password', 'value'),
-    Input('Repassword_form','n_clicks')])
-def update_username_info(Repassword,Password,n_click):
-    if not Repassword:
-        raise PreventUpdate
-    if not re.search(r'^(?![a-zA-z]+$)(?!\d+$)(?![!@#$%^&*.,?]+$)(?![a-z\d]+$)(?![A-Z\d]+$)(?![a-z!@#$%^&*,.?]+$)(?![A-Z!@#$%^&*,.?]+$)(?![\d!@#$%^&*,.?]+$)[a-zA-Z\d!@#$%^&*,.?]{8,25}$',Repassword):
-        return html.Div('The password input format is wrong, please input according to the rules!')
-    if Password != Repassword and n_click>1:
-        return html.Div('The two password entries are different, please re-enter!')
+        return [{'label': 'None', 'value': 'None'}]
 
 @app.callback(
     Output('account_result', 'children'),
@@ -149,19 +60,21 @@ def update_account(n_clicks,Username, Password,RePassword,Realname,Email,Phone,D
     conn = Users(username=Username)
     if Username and Password and RePassword and Email and Organization and n_clicks > 0:
         update_info = Users(Username, '',Realname,Email,Phone,District,Country,City,Organization)
-        if not re.search(r'^[a-zA-Z]+[a-zA-Z0-9_]*',Username):
+        if not re.search(r'^[0-9a-zA-Z_]+$',Username):
             return html.Div('The username is incorrect, please re-enter!')
         else:
             res=conn.checkExists()
             if res:
                 return html.Div('Username already exists, please re-enter!')
-        if not re.search(r'^(?![a-zA-z]+$)(?!\d+$)(?![!@#$%^&*.,?]+$)(?![a-z\d]+$)(?![A-Z\d]+$)(?![a-z!@#$%^&*,.?]+$)(?![A-Z!@#$%^&*,.?]+$)(?![\d!@#$%^&*,.?]+$)[a-zA-Z\d!@#$%^&*,.?]{8,25}$',Password) and not re.search(r'^(?![a-zA-z]+$)(?!\d+$)(?![!@#$%^&*.,?]+$)(?![a-z\d]+$)(?![A-Z\d]+$)(?![a-z!@#$%^&*,.?]+$)(?![A-Z!@#$%^&*,.?]+$)(?![\d!@#$%^&*,.?]+$)[a-zA-Z\d!@#$%^&*,.?]{8,25}$',RePassword):
+        if not re.search(r'^(?=.*[a-zA-Z])(?=.*[1-9])(?=.*[\W]).{8,25}$',Password) and not re.search(r'^(?=.*[a-zA-Z])(?=.*[1-9])(?=.*[\W]).{8,25}$',RePassword):
             return html.Div('The password input format is wrong, please input according to the rules!')
         if Password != RePassword:
             return html.Div('The two password entries are different, please re-enter!')
+        if not re.search(r'^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$',Email):
+            return html.Div('The email format is wrong, please re-enter!')
         update_info.add()
         update_info.password = Password
-        return html.Div('Registration is successful, click Cancel to return to login.')
+        return html.Div('Registration is successful, click Return to login.')
     elif n_clicks >= 1:
         return html.Div('*Part of the content is required, please fill in completely!')
 
@@ -173,12 +86,15 @@ def update_account(n_clicks,Username, Password,RePassword,Realname,Email,Phone,D
     Output('Realname','value'),
     Output('Email','value'),
     Output('Phone','value'),
+    Output('District','value'),
+    Output('Country','value'),
+    Output('Province','value'),
+    Output('City','value'),
     Output('Organization','value')],
     [Input('clear_button', 'n_clicks')])
 def clear_input(n_clicks):
     if not n_clicks:
         raise PreventUpdate
     if n_clicks > 0:
-        return '','','','','','',''
-        
+        return '','','','','','','','','','',''    
     
