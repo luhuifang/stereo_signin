e6594e1727d9807a5770a529cfd8743b2f682434
zhou995287902 <zhouliangliang@users.noreply.github.com>
2020-08-31 11:30:25 +0800

    verification code time
    add captcha remains the same for 30 minutes

--- apps/login/email_setting.py
+++
@@ -1,6 +0,0 @@
-EMAIL_HOST = 'smtp.qq.com' 
-EMAIL_PORT = 465               
-EMAIL_HOST_USER = '1972845691@qq.com'   
-EMAIL_HOST_PASSWORD = 'csfdiokkndlfeifj'  
-EMAIL_USE_TLS = False             
-EMAIL_FROM = '1972845691@qq.com'        
